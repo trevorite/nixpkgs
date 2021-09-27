9cae0da06e3b62b8fa5b28de49680e96844cd335
Trevor Lee <trevor@momenti.tv>
2021-09-27 17:27:32 +0900

    R24.1

--- pkgs/development/interpreters/erlang/R24.nix
+++ pkgs/development/interpreters/erlang/R24.nix
@@ -3,7 +3,7 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "24.0.6";
-  sha256 = "0z01hkzf2y6lz20s2vkn4q874lb6n6j00jkbgk4gg60rhrmq904z";
+  version = "24.1";
+  sha256 = "1zxiqilnjrja2ihrsnpzlz2achkws1b7dnliw5qnzvz2sn9gf6fx";
   configureFlags = [ "--disable-jit" ];
 }
